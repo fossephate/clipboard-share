a067021f09ceae35f8c1fdf71a381d0a3ce3ff75
fossephate <matt.cfosse@gmail.com>
2024-07-18 11:01:02 -0700

    save

--- main.py
+++ main.py
@@ -4,31 +4,49 @@ import time
 import argparse
 
 def read_clipboard_to_file(filename):
+    if not os.path.exists(filename):
+        print(f"File {filename} not found")
+        return
     clipboard_content = pyperclip.paste()
     with open(filename, 'w', encoding='utf-8') as f:
         f.write(clipboard_content)
     print(f"Clipboard content saved to {filename}")
 
 def write_file_to_clipboard(filename):
-    if os.path.exists(filename):
-        with open(filename, 'r', encoding='utf-8') as f:
-            file_content = f.read()
-        pyperclip.copy(file_content)
-        print(f"Content from {filename} copied to clipboard")
-    else:
+    if not os.path.exists(filename):
         print(f"File {filename} not found")
+        return
+
+    with open(filename, 'r', encoding='utf-8') as f:
+        file_content = f.read()
+    pyperclip.copy(file_content)
+    print(f"Content from {filename} copied to clipboard")
+
+def read_file(filename):
+    if not os.path.exists(filename):
+        print(f"File {filename} not found")
+        return
+    with open(filename, 'r', encoding='utf-8') as f:
+        file_content = f.read()
+        return file_content
 
 def monitor_clipboard(filename):
-    last_content = pyperclip.paste()
+    last_clip_content = pyperclip.paste()
+    # last_file_content = read_file(filename)
     print("Monitoring clipboard. Press Ctrl+C to stop.")
     try:
         while True:
-            current_content = pyperclip.paste()
-            if current_content != last_content:
+            current_clip_content = pyperclip.paste().replace('\r\n', '\n')
+            if current_clip_content != last_clip_content:
                 with open(filename, 'w', encoding='utf-8') as f:
-                    f.write(current_content)
+                    f.write(current_clip_content)
                 print(f"New clipboard content saved to {filename}")
-                last_content = current_content
+                last_clip_content = current_clip_content
+            
+            current_file_content = read_file(filename)
+            if current_clip_content != current_file_content:
+                write_file_to_clipboard(filename)
+            
             time.sleep(1)
     except KeyboardInterrupt:
         print("Clipboard monitoring stopped")
