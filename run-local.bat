7cae634f7ef4092fe643348a58b12893b7f804a0
fossephate <matt.cfosse@gmail.com>
2024-09-13 12:54:29 -0700

    make batch file restart on failure

--- run-local.bat
+++ run-local.bat
@@ -1,1 +1,8 @@
-python -m pipenv run python main.py W:\\clip.txt
+@echo off
+:loop
+python -m pipenv run python main.py W:\clip.txt
+if %ERRORLEVEL% neq 0 (
+    echo Script crashed with exit code %ERRORLEVEL%. Restarting...
+    timeout /t 5
+    goto loop
+)
